8bbefefdd2504765e4d1080c759a49c93bf5923f
thinkJD <jd.georgens@gmail.com>
2023-10-30 17:34:55 +0100

    use table writer to render menu table

--- cmd/get_menue.go
+++ cmd/get_menue.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
@@ -12,16 +13,28 @@ var getMenue = &cobra.Command{
 	Use:   "get-menue",
 	Short: "TasteNext account login",
 	Run: func(cmd *cobra.Command, args []string) {
-		menues, err := cli.GetMenue()
+		menus, err := cli.GetMenue()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		jsonData, err := json.MarshalIndent(menues, "", "    ")
-		if err != nil {
-			log.Fatal(err)
+		// Initialize tablewriter
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"kw", "Name", "Description"})
+
+		// Iterate through the list and append each dish's name and description to the table
+		for _, menu := range menus {
+			calendarWeek := menu.MenuBlockWeekWrapper.MenuBlockWeek.CalendarWeek
+			for _, week := range menu.MenuBlockWeekWrapper.MenuBlockWeek.MenuBlockLineWeeks {
+				for _, entry := range week.Entries {
+					dishName := entry.Dish.Name
+					dishDescription := entry.Dish.Description
+					table.Append([]string{fmt.Sprintf("%d", calendarWeek), dishName, dishDescription})
+				}
+			}
 		}
-		fmt.Println(string(jsonData))
+
+		table.Render()
 	},
 }
 
