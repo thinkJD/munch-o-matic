43d9197127b1b319b13dd79a8f1ddf6b0a38d5e4
thinkJD <jd.georgens@gmail.com>
2023-10-30 12:49:34 +0000

    refactor requests

--- client/client.go
+++ client/client.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"mime/multipart"
 	"net/http"
 	"net/http/cookiejar"
@@ -60,6 +61,35 @@ func NewClient(config Config) (*RestClient, error) {
 	return c, nil
 }
 
+func (c *RestClient) SendRequest(method, urlStr string, body io.Reader, result interface{}) error {
+	if c.Client == nil {
+		return fmt.Errorf("client not initialized. Please login first")
+	}
+
+	req, err := http.NewRequest(method, urlStr, body)
+	if err != nil {
+		return fmt.Errorf("error creating request: %v", err)
+	}
+
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error performing request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %v", err)
+	}
+
+	err = json.Unmarshal(respBody, result)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling JSON: %v", err)
+	}
+
+	return nil
+}
+
 // Login performs a login operation and stores the sessionID.
 func (c *RestClient) Login(config Config) error {
 	// Prepare the multipart form data
@@ -105,33 +135,9 @@ func (c *RestClient) Login(config Config) error {
 func (c *RestClient) GetCurrentUser() (CurrentUserResponse, error) {
 	var currentUserResp CurrentUserResponse
 
-	if c.Client == nil {
-		return CurrentUserResponse{}, fmt.Errorf("client not initialized. Please login first")
-	}
-
-	req, err := http.NewRequest("GET", "https://rest.tastenext.de/backend/user/current-user", nil)
+	err := c.SendRequest("GET", "https://rest.tastenext.de/backend/user/current-user", nil, &currentUserResp)
 	if err != nil {
 		return CurrentUserResponse{}, fmt.Errorf("error creating request: %v", err)
-	}
-
-	resp, err := c.Client.Do(req)
-	if err != nil {
-		return CurrentUserResponse{}, fmt.Errorf("error performing request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return CurrentUserResponse{}, fmt.Errorf("Server returned status code %v", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return CurrentUserResponse{}, fmt.Errorf("error reading response body: %v", err)
-	}
-
-	err = json.Unmarshal(body, &currentUserResp)
-	if err != nil {
-		return CurrentUserResponse{}, fmt.Errorf("error unmarshaling JSON: %v", err)
 	}
 
 	return currentUserResp, nil
@@ -140,62 +146,40 @@ func (c *RestClient) GetCurrentUser() (CurrentUserResponse, error) {
 func (c *RestClient) GetUser() (UserResponse, error) {
 	var userResp UserResponse
 
-	if c.Client == nil {
-		return userResp, fmt.Errorf("client not initialized. Please login first")
-	}
-
 	urlWithUserID := fmt.Sprintf("https://rest.tastenext.de/backend/user/%d", c.UserId)
-
-	req, err := http.NewRequest("GET", urlWithUserID, nil)
+	err := c.SendRequest("GET", urlWithUserID, nil, &userResp)
 	if err != nil {
-		return UserResponse{}, fmt.Errorf("error creating request: %v", err)
+		log.Fatal("Error sending request")
 	}
-
-	resp, err := c.Client.Do(req)
-	if err != nil {
-		return UserResponse{}, fmt.Errorf("error performing request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return UserResponse{}, fmt.Errorf("error reading response body: %v", err)
-	}
-
-	err = json.Unmarshal(body, &userResp)
-	if err != nil {
-		return UserResponse{}, fmt.Errorf("error unmarshaling JSON: %v", err)
-	}
-
-	// Pretty-print the JSON
-	prettyJSON, err := json.MarshalIndent(userResp, "", "  ")
-	if err != nil {
-		return UserResponse{}, fmt.Errorf("error marshaling JSON: %v", err)
-	}
-
-	fmt.Println(string(prettyJSON))
-
 	return userResp, nil
 }
 
 func (c *RestClient) GetMenue() (MenuResponse, error) {
-	var menuResponse MenuResponse
+	var menuResponses []MenuResponse
 
 	if c.Client == nil {
 		return MenuResponse{}, fmt.Errorf("client not initialized. Please login first")
 	}
 	customer := 44897 // TODO: get this from the user object
-
 	nextWeeks := getNextFourWeeks()
 
-	menuUrl := fmt.Sprintf(
-		"https://rest.tastenext.de/frontend/menu/get-personal-menu-week/calendar-week/%d/year/%d/customer/%d/menu-block/14",
-		nextWeeks[0].CalendarWeek,
-		nextWeeks[0].Year,
-		customer,
-	)
+	for _, week := range nextWeeks {
+		menuUrl := fmt.Sprintf(
+			"https://rest.tastenext.de/frontend/menu/get-personal-menu-week/calendar-week/%d/year/%d/customer/%d/menu-block/14",
+			week.CalendarWeek,
+			week.Year,
+			customer,
+		)
 
-	fmt.Println(menuUrl)
+		var menuResp MenuResponse
+		err := c.SendRequest("GET", menuUrl, nil, &menuResp)
+		if err != nil {
+			log.Fatal("Error getting menues")
+		}
+
+		menuResponses.append(menuResp)
+		fmt.Print()
+	}
 
 	return menuResponse, nil
 }
