4116c616cdac343fb0f2ee1ffe069b61c16eee69
thinkJD <jd.georgens@gmail.com>
2023-11-02 17:09:25 +0100

    chore(app): add input validation to menu order cmd

--- cmd/menu_order.go
+++ cmd/menu_order.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -12,8 +13,13 @@ var cancelOrder bool
 
 var orderMenu = &cobra.Command{
 	Use:   "order",
-	Short: "order menu",
+	Short: "Order or cancel a dish from the menu",
 	Run: func(cmd *cobra.Command, args []string) {
+		if dishId == 0 {
+			fmt.Println("Error: The --dish flag is mandatory")
+			cmd.Usage()
+			os.Exit(1)
+		}
 		err := cli.OrderMenu(dishId, cancelOrder)
 		if err != nil {
 			log.Fatal(err)
@@ -23,8 +29,8 @@ var orderMenu = &cobra.Command{
 }
 
 func init() {
-	orderMenu.Flags().IntVarP(&dishId, "dish", "d", 0, "Dish id to order")
-	orderMenu.Flags().BoolVar(&cancelOrder, "c", false, "Cancel the order ")
+	orderMenu.Flags().IntVarP(&dishId, "dish", "d", 0, "DishId to order")
+	orderMenu.Flags().BoolVar(&cancelOrder, "c", false, "Cancel the dish")
 
 	menuCmd.AddCommand(orderMenu)
 }
