27c563d1a395fc850f15ba5b4d385181cf55aa21
Jan Georgens <jd.georgens@gmail.com>
2023-12-27 16:07:41 +0100

    feat: Add low balance notification (#28)

---
+++ cmd/notification.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"log"
+	"munch-o-matic/core"
+
+	"github.com/spf13/cobra"
+)
+
+var notification = &cobra.Command{
+	Use:   "send-notification",
+	Short: "Send notifications",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		err := core.SendNotification(ntfyTopic, title, message)
+		if err != nil {
+			log.Fatal("could not send notification: ", err)
+		}
+	},
+}
+
+func init() {
+	notification.Flags().StringVarP(&ntfyTopic, "topic", "T", "", "Ntfy topic")
+	notification.Flags().StringVarP(&title, "title", "t", "CLI", "Notification title")
+	notification.Flags().StringVarP(&message, "message", "m", "", "Notificatoin message")
+	rootCmd.AddCommand(notification)
+}
---
+++ core/notification.go
@@ -0,0 +1,46 @@
+package core
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+// SendTemplateNotification takes a template and matching sate to render the template and
+// send to the provided topic.
+func SendTemplateNotification(Topic string, Title string, Template string, Data interface{}) error {
+	tmpl, err := template.New("template").Parse(Template)
+	if err != nil {
+		return fmt.Errorf("create template: %w", err)
+	}
+	var buf bytes.Buffer
+
+	err = tmpl.Execute(&buf, Data)
+	if err != nil {
+		return fmt.Errorf("execute template: %w", err)
+	}
+	renderedTemplate := buf.String()
+	err = SendNotification(Topic, Title, renderedTemplate)
+	if err != nil {
+		return fmt.Errorf("sending failed: %w", err)
+	}
+	return nil
+}
+
+// SendNotification sends the provided message to the topic
+func SendNotification(Topic string, Title string, Message string) error {
+	req, _ := http.NewRequest("POST", "https://ntfy.sh/"+Topic, strings.NewReader(Message))
+	req.Header.Set("Title", Title)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("could not send notification: %w")
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("send notification failed with status %s", resp.Status)
+	}
+
+	return nil
+}
