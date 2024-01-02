8935c7e209a0db6fff40a1f0e6cc96630b2faa94
thinkJD <jd.georgens@gmail.com>
2024-01-02 14:19:00 +0100

    resructure channels

--- core/daemon.go
+++ core/daemon.go
@@ -11,15 +11,32 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
+type jobStatus struct {
+	JobId string
+	Msg   string
+	Err   error
+}
+
+type jobNotification struct {
+	JobId    string
+	Title    string
+	Template string
+	Data     interface{}
+}
+
 type Daemon struct {
-	d     *Daemon
-	chron *cron.Cron
-	cli   *client.RestClient
-	cfg   Config
+	chron            *cron.Cron
+	cli              *client.RestClient
+	cfg              Config
+	statusChan       chan jobStatus
+	notificationChan chan jobNotification
 }
 
 func NewDaemon(Cfg Config, Cli *client.RestClient) (*Daemon, error) {
-	retVal := Daemon{}
+	retVal := Daemon{
+		statusChan:       make(chan jobStatus),
+		notificationChan: make(chan jobNotification),
+	}
 
 	err := ValidateConfig(Cfg)
 	if err != nil {
@@ -33,62 +50,31 @@ func NewDaemon(Cfg Config, Cli *client.RestClient) (*Daemon, error) {
 	return &retVal, nil
 }
 
-func (d *Daemon) AddJob(StatusChan chan jobStatus, Job Job) error {
-	switch Job.Type {
-	case "CheckBalance":
-		minBalance, ok1 := Job.Params["minbalance"].(int)
-		template, ok2 := Job.Params["template"].(string)
-		if !ok1 || !ok2 {
-			return fmt.Errorf("invalid parameter types for CheckBalance")
-		}
-
-		_, err := d.chron.AddFunc(Job.Schedule, d.sendLowBalanceNotification(StatusChan, minBalance, template))
-		if err != nil {
-			return fmt.Errorf("error adding job: %w", err)
-		}
-
-	case "Order":
-		strategy, ok1 := Job.Params["strategy"].(string)
-		weeks, ok2 := Job.Params["weeks"].(int)
-		if !ok1 || !ok2 {
-			return fmt.Errorf("invalid parameter types for Order")
-		}
-		_, err := d.chron.AddFunc(Job.Schedule, d.orderFood(StatusChan, strategy, weeks))
-		if err != nil {
-			return fmt.Errorf("error adding job: %w", err)
-		}
-
-	case "UpdateMetrics":
-		_, err := d.chron.AddFunc(Job.Schedule, d.updateMetrics(StatusChan))
-		if err != nil {
-			return fmt.Errorf("error adding job: %w", err)
-		}
-
-	default:
-		return fmt.Errorf("%v is not a valid type", Job.Type)
-	}
-	return nil
-}
-
-type jobStatus struct {
-	JobId string
-	Msg   string
-	Err   error
-}
-
 func (d Daemon) Run() error {
 	stopChan := make(chan os.Signal, 1)
 	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
 
-	statusChan := make(chan jobStatus)
-
 	for _, job := range d.cfg.DaemonConfiguration.Jobs {
-		d.AddJob(statusChan, job)
+		d.AddJob(job)
 	}
+
+	// Handle notifications
+	go func() {
+		for msg := range d.notificationChan {
+			// Skip if the status notification is disabled
+			if !d.cfg.DaemonConfiguration.Notification.Status.Enabled {
+				continue
+			}
+
+			SendTemplateNotification(
+				d.cfg.DaemonConfiguration.Notification.Status.Topic,
+				msg.Title, msg.Template, msg.Data)
+		}
+	}()
 
 	// Handle status updates and errors
 	go func() {
-		for msg := range statusChan {
+		for msg := range d.statusChan {
 			if msg.Err != nil {
 				err := fmt.Errorf("error in job=%s\t%w", msg.JobId, msg.Err)
 				if d.cfg.DaemonConfiguration.Notification.Error.Enabled {
@@ -115,7 +101,7 @@ func (d Daemon) Run() error {
 		fmt.Println("Shutting down...")
 
 		d.chron.Stop()
-		close(statusChan)
+		close(d.statusChan)
 
 		fmt.Println("Shutdown complete")
 		os.Exit(0)
@@ -127,22 +113,59 @@ func (d Daemon) Run() error {
 
 	// Block here until interrupted
 	select {}
+}
 
+func (d *Daemon) AddJob(Job Job) error {
+	switch Job.Type {
+	case "CheckBalance":
+		minBalance, ok1 := Job.Params["minbalance"].(int)
+		template, ok2 := Job.Params["template"].(string)
+		if !ok1 || !ok2 {
+			return fmt.Errorf("invalid parameter types for CheckBalance")
+		}
+
+		_, err := d.chron.AddFunc(Job.Schedule, d.sendLowBalanceNotification(minBalance, template))
+		if err != nil {
+			return fmt.Errorf("error adding job: %w", err)
+		}
+
+	case "Order":
+		strategy, ok1 := Job.Params["strategy"].(string)
+		weeks, ok2 := Job.Params["weeks"].(int)
+		if !ok1 || !ok2 {
+			return fmt.Errorf("invalid parameter types for Order")
+		}
+		_, err := d.chron.AddFunc(Job.Schedule, d.orderFood(strategy, weeks))
+		if err != nil {
+			return fmt.Errorf("error adding job: %w", err)
+		}
+
+	case "UpdateMetrics":
+		_, err := d.chron.AddFunc(Job.Schedule, d.updateMetrics())
+		if err != nil {
+			return fmt.Errorf("error adding job: %w", err)
+		}
+
+	default:
+		return fmt.Errorf("%v is not a valid type", Job.Type)
+	}
+	return nil
 }
 
 // Jobs
 // ##########
-func (d Daemon) orderFood(ch chan jobStatus, Strategy string, WeeksInAdvance int) func() {
+
+func (d Daemon) orderFood(Strategy string, WeeksInAdvance int) func() {
 	return func() {
 		var jobId = "orderFood"
 		menu, err := d.cli.GetMenuWeeks(WeeksInAdvance)
 		if err != nil {
-			ch <- jobStatus{JobId: jobId, Err: fmt.Errorf("trouble getting menu: %v", err.Error())}
+			d.statusChan <- jobStatus{JobId: jobId, Err: fmt.Errorf("trouble getting menu: %v", err.Error())}
 		}
 
 		dishes, err := ChooseDishesByStrategy(Strategy, menu)
 		if err != nil {
-			ch <- jobStatus{JobId: jobId, Err: fmt.Errorf("Trouble choosing dish: %v", err.Error())}
+			d.statusChan <- jobStatus{JobId: jobId, Err: fmt.Errorf("Trouble choosing dish: %v", err.Error())}
 		}
 		fmt.Println(dishes)
 		/*
@@ -153,36 +176,40 @@ func (d Daemon) orderFood(ch chan jobStatus, Strategy string, WeeksInAdvance int
 				}
 			}
 		*/
-		ch <- jobStatus{JobId: jobId, Msg: fmt.Sprintf("Food ordered with %v strategy, for %v weeks", Strategy, WeeksInAdvance)}
+		d.statusChan <- jobStatus{JobId: jobId, Msg: fmt.Sprintf("Food ordered with %v strategy, for %v weeks", Strategy, WeeksInAdvance)}
 	}
 }
 
-func (d Daemon) sendLowBalanceNotification(ch chan jobStatus, MinBalance int, Template string) func() {
+func (d Daemon) sendLowBalanceNotification(MinBalance int, Template string) func() {
 	return func() {
 		var jobId = "sendLowBalanceNotification"
-		ch <- jobStatus{JobId: jobId, Msg: "Checking account balance"}
+		d.statusChan <- jobStatus{JobId: jobId, Msg: "Checking account balance"}
 
 		user, err := d.cli.GetUser()
 		if err != nil {
-			ch <- jobStatus{JobId: jobId, Err: fmt.Errorf("trouble getting user details: %v", err.Error())}
+			d.statusChan <- jobStatus{JobId: jobId, Err: fmt.Errorf("trouble getting user details: %v", err.Error())}
 		}
 
 		if user.User.Customer.AccountBalance.Amount <= MinBalance {
-			ch <- jobStatus{JobId: jobId, Msg: "Account balance below minimum"}
-
-			SendTemplateNotification(d.cfg.DaemonConfiguration.Notification.Status.Topic, "Low balance notification", Template, user)
+			d.statusChan <- jobStatus{JobId: jobId, Msg: "Account balance below minimum"}
+			// Send notification
+			d.notificationChan <- jobNotification{
+				JobId:    jobId,
+				Title:    "Low Balance",
+				Template: Template,
+				Data:     user}
 		}
 
 	}
 }
 
-func (d Daemon) updateMetrics(ch chan jobStatus) func() {
+func (d Daemon) updateMetrics() func() {
 	return func() {
 		var jobId = "updateMetrics"
-		ch <- jobStatus{JobId: jobId, Msg: "Update metrics"}
+		d.statusChan <- jobStatus{JobId: jobId, Msg: "Update metrics"}
 		menuWeeks, err := d.cli.GetMenuWeeks(4)
 		if err != nil {
-			ch <- jobStatus{JobId: jobId, Err: fmt.Errorf("could not load dishes: %w", err)}
+			d.statusChan <- jobStatus{JobId: jobId, Err: fmt.Errorf("could not load dishes: %w", err)}
 			return
 		}
 
@@ -197,7 +224,7 @@ func (d Daemon) updateMetrics(ch chan jobStatus) func() {
 
 		user, err := d.cli.GetUser()
 		if err != nil {
-			ch <- jobStatus{JobId: jobId, Err: fmt.Errorf("could not load user: %w", err)}
+			d.statusChan <- jobStatus{JobId: jobId, Err: fmt.Errorf("could not load user: %w", err)}
 			return
 		}
 		UpdateAccountBalance(user.User.ID, user.User.FirstName, user.User.Customer.AccountBalance.Amount)
